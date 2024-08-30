212db9c3b915980268c14f2e41982f590bb7804e
Liren4396 <93173788+Liren4396@users.noreply.github.com>
2024-08-30 13:25:53 +0800

    Add files via upload

---
+++ exercises-w9/build/CMakeFiles/ring.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/import/glass/5/z5369144/Downloads/comp6771/exercises-w9/src/9.3/ring.cpp" "CMakeFiles/ring.dir/src/9.3/ring.cpp.o" "gcc" "CMakeFiles/ring.dir/src/9.3/ring.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/import/glass/5/z5369144/Downloads/comp6771/exercises-w9/build/CMakeFiles/catch2_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ exercises-w9/build/CMakeFiles/ring.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ring.dir/src/9.3/ring.cpp.o"
+  "CMakeFiles/ring.dir/src/9.3/ring.cpp.o.d"
+  "ring"
+  "ring.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
